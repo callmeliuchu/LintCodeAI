2f517dbfdbe6b967ce1a397820c3138aa222bcb2
lichu <1371772034@qq.com>
2018-01-18 23:34:37 +0800

    ai

--- MessageClassfy1/code/bayes.py
+++ MessageClassfy1/code/bayes.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+
+
+
 def loadDataSet():
     postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                  ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
